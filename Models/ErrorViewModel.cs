088568ca07e9fcd28662548ef388cd62d1e014f2
Alphadrake86 <anderson8221@student.cptc.edu>
2020-09-30 10:40:14 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CraftsMadeByHand.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
