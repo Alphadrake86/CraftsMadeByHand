9a18d35060a8bde442e6017b084182b705995adc
Erik <anderson8221@student.cptc.edu>
2020-11-02 18:42:16 -0800

    Added image class

---
+++ Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftsMadeByHand.Models
+{
+    /// <summary>
+    /// Represents an image that is stored on the disk. The path is 
+    /// stored on the database for ease of retrieval.
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        /// The image path on the disk
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// The product that this image is for
+        /// </summary>
+        public int ProductId { get; set; }
+    }
+}
