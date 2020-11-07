7175c02f0650944c01ee9d550e47b0e49f2b07d3
Erik <anderson8221@student.cptc.edu>
2020-11-07 14:29:04 -0800

    added a helper class for identity options

---
+++ Models/IdentityHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftsMadeByHand.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // Sign In options
+            options.SignIn.RequireConfirmedAccount = true;
+            options.SignIn.RequireConfirmedEmail = true;
+
+            // Password options
+            options.Password.RequireDigit = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredLength = 8;
+
+
+        }
+    }
+}
